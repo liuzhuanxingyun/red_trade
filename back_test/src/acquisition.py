061eb9f1abbb464d65547b4e2059b3e8177b10c1
liuzhuanxingyun <2160255989@qq.com>
2025-11-04 08:22:01 +0800

    优化了回测数据下载函数，重新显示开发手册

--- back_test/src/acquisition.py
+++ back_test/src/acquisition.py
@@ -5,33 +5,31 @@ from .utils import load_and_process_data, download_binance_data, unzip_binance_d
 def acquire_data(symbol, interval, selected_years=None, selected_months=None, save_dir='back_test/data'):
 
     if selected_years and selected_months:
-        # 检查所需子文件是否存在
-        files_exist = True
-        for year in selected_years:
-            for month in selected_months:
-                file_path = f'{save_dir}/{symbol}-{interval}/{symbol}-{interval}-{year}-{month:02d}.csv'
-                if not os.path.exists(file_path):
-                    files_exist = False
-                    break
-            if not files_exist:
-                break
-        
-        if not files_exist:
-            # 下载并解压数据
-            download_binance_data(symbol=symbol, interval=interval, years=selected_years, months=selected_months, save_dir=save_dir)
-            unzip_binance_data(symbol=symbol, interval=interval, save_dir=save_dir)
-            delete_zip_folder(symbol, interval, save_dir)
-        
-        # 合并并加载数据
-        input_dir = f'{save_dir}/{symbol}-{interval}'
-        # --- 改进：创建基于年月的文件名 ---
+        # --- 改进：优先检查合并文件是否存在 ---
         years_str = '_'.join(map(str, selected_years))
         months_str = '_'.join(map(str, selected_months))
         output_file = f'{save_dir}/merged_{symbol}-{interval}_{years_str}_{months_str}.csv'
-        # --- 结束改进 ---
         
-        # 仅在合并文件不存在时才执行合并操作
         if not os.path.exists(output_file):
+            # 检查所需子文件是否存在
+            files_exist = True
+            for year in selected_years:
+                for month in selected_months:
+                    file_path = f'{save_dir}/{symbol}-{interval}/{symbol}-{interval}-{year}-{month:02d}.csv'
+                    if not os.path.exists(file_path):
+                        files_exist = False
+                        break
+                if not files_exist:
+                    break
+            
+            if not files_exist:
+                # 下载并解压数据
+                download_binance_data(symbol=symbol, interval=interval, years=selected_years, months=selected_months, save_dir=save_dir)
+                unzip_binance_data(symbol=symbol, interval=interval, save_dir=save_dir)
+                delete_zip_folder(symbol, interval, save_dir)
+            
+            # 合并数据
+            input_dir = f'{save_dir}/{symbol}-{interval}'
             print(f"合并文件 {output_file} 不存在，开始合并...")
             merge_csv_files_by_years_months(
                 symbol=symbol, interval=interval, years=selected_years, months=selected_months,
